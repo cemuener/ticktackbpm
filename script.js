7fd79021bb722915ee0178556e34c44dd3336680
cuener <cuener@eggs.de>
2024-08-20 14:08:09 +0200

    new styling

--- script.js
+++ script.js
@@ -8,21 +8,17 @@ const countdownInterval = setInterval(function() {
     const now = new Date().getTime();
     const timeRemaining = targetDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    if (timeRemaining >= 0) {
+        // Time calculations for days, hours, minutes and seconds
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="countdown"
-    document.getElementById('countdown').textContent = 
-        (days < 10 ? "0" + days : days) + "d " + 
-        (hours < 10 ? "0" + hours : hours) + "h " + 
-        (minutes < 10 ? "0" + minutes : minutes) + "m " + 
-        (seconds < 10 ? "0" + seconds : seconds) + "s ";
-
-    // If the countdown is finished, write some text
-    if (timeRemaining < 0) {
+        // Display the result in the element with id="countdown"
+        document.getElementById('countdown').textContent = 
+            `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    } else {
         clearInterval(countdownInterval);
         document.getElementById('countdown').textContent = "EXPIRED";
     }
